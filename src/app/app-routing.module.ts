41d8e174deb85df290e544ad0bf857440c5faf8f
meyond <332595512@qq.com>
2019-07-11 23:51:15 +0800

    fixed warning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product/:prodTitle', component: ProductDetailComponent },
+  { path: '**', component: Code404Component },
 ];
 
 @NgModule({
